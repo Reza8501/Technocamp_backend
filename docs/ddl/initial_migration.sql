59771544fe0a8f9fd4757ba34d3100ca80ad18b8
bayudava16 <bayudava16@gmail.com>
2023-10-21 18:27:46 +0700

    docs(ddl):add initial migration

---
+++ docs/ddl/initial_migration.sql
@@ -0,0 +1,4 @@
+insert into roles (`name`) values ('admin');
+insert into roles (`name`) values ('client');
+
+insert into users (full_name, username, email, `password`, `role`, created_at, updated_at) values ('admin', 'admin', 'admin@gmail.com', '$2a$12$1yywWzt1MfghWc5v/yUTkOPKaTyGMNcup8smCqSR24oBQDKXpVZYK', 'admin', now(), now());
