1cc47414a0642497d66d00b3bbc99aab3c82776e
bayudava16 <bayudava16@gmail.com>
2023-10-21 22:51:16 +0700

    usecase(register_user):fix sent email

--- usecase/u_register_user.go
+++ usecase/u_register_user.go
@@ -19,7 +19,12 @@ func (u *usecase) RegisterUser(c context.Context, req dto.RequestRegisterUser) *
 		return response.BuildInternalErrorResponse(response.ERROR_CODE_DATABASE_ERROR, response.RESPONSE_CODE_INTERNAL_ERROR, "Error getting user", errUser.Error())
 	}
 
-	if user != nil {
+	userByEmail, errUserByEmail := u.Repo.GetUserByUsername(c, req.Email)
+	if errUserByEmail != nil && !gorm.IsRecordNotFoundError(errUserByEmail) {
+		return response.BuildInternalErrorResponse(response.ERROR_CODE_DATABASE_ERROR, response.RESPONSE_CODE_INTERNAL_ERROR, "Error getting user", errUserByEmail.Error())
+	}
+
+	if user != nil && userByEmail != nil {
 		return response.BuildBadRequestResponse(response.ERROR_CODE_INVALID_BODY_REQUEST, response.RESPONSE_CODE_BAD_REQUEST, "Data already exist", "User has been registered")
 	}
 
@@ -35,7 +40,7 @@ func (u *usecase) RegisterUser(c context.Context, req dto.RequestRegisterUser) *
 		if err != nil {
 			log.Print(err.Error())
 		} else {
-			log.Print("apa kek send email sukses dah gitu dah terserah")
+			log.Print("Successfully send verification email")
 		}
 	}()
 
@@ -45,7 +50,13 @@ func (u *usecase) RegisterUser(c context.Context, req dto.RequestRegisterUser) *
 func SendVerificationEmail(email string) error {
 	auth := smtp.PlainAuth("", viper.GetString("SMTP_USERNAME"), viper.GetString("SMTP_PASSWORD"), viper.GetString("SMTP_HOST"))
 
-	err := smtp.SendMail(fmt.Sprintf("%s:%s", viper.GetString("SMTP_HOST"), viper.GetString("SMTP_PORT")), auth, "bayudava16@gmail.com", []string{email}, []byte("testing"))
+	// build message
+	message := fmt.Sprintf("From: %s\r\n", viper.GetString("APP_EMAIL"))
+	message += fmt.Sprintf("To: %s\r\n", email)
+	message += fmt.Sprintf("Subject: %s\r\n", "Testing")
+	message += fmt.Sprintf("\r\n%s\r\n", "Testing")
+
+	err := smtp.SendMail(fmt.Sprintf("%s:%s", viper.GetString("SMTP_HOST"), viper.GetString("SMTP_PORT")), auth, viper.GetString("APP_EMAIL"), []string{email}, []byte(message))
 	if err != nil {
 		return err
 	}
