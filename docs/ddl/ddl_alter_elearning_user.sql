0122ce5c16cc27d935dc046aac5fcf1e553e6ae6
bayudava16 <bayudava16@gmail.com>
2023-10-30 18:32:03 +0700

    docs(ddl):update ddl

--- docs/ddl/ddl_alter_elearning_user.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ta.users
-ADD COLUMN full_name varchar(255);
-ADD COLUMN status_verification boolean default false;
-ADD COLUMN created_at timestamp;
-ADD COLUMN updated_at timestamp;
-ADD COLUMN deleted_at timestamp default null;
-
-ALTER TABLE ta.courses
-ADD COLUMN price int(255)
