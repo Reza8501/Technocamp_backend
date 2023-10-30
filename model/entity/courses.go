c72c2c8b3fc8aba5d0b38ae0079512af6c66ad6b
bayudava16 <bayudava16@gmail.com>
2023-10-30 16:48:45 +0700

    output(manual_transaction):add item relation

--- model/entity/courses.go
+++ model/entity/courses.go
@@ -8,6 +8,7 @@ type Courses struct {
 	IsFree        bool   `json:"isFree" gorm:"column:is_free"`
 	IsActive      bool   `json:"isActive" gorm:"column:is_active"`
 	HasBuyer      bool   `json:"hasBuyer" gorm:"column:has_buyer"`
+	Price         int    `json:"price" gorm:"column:price"`
 }
 
 type CoursesOutput struct {
@@ -15,4 +16,5 @@ type CoursesOutput struct {
 	CourseTitle   string `json:"courseTitle" gorm:"column:course_title"`
 	CourseImage   string `json:"courseImage" gorm:"column:course_image"`
 	CourseContent string `json:"courseContent" gorm:"column:course_content"`
+	Price         int    `json:"price" gorm:"column:price"`
 }
