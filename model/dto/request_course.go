8758b30f5a902a9d2eeb81cc7a5f621cb47cd9ce
Sharing Vision <dani.rahmat7398@gmail.com>
2023-10-31 08:20:59 +0700

    update json

--- model/dto/request_course.go
+++ model/dto/request_course.go
@@ -13,20 +13,20 @@ type ReqDeleteCourseById struct {
 
 type ReqUpdateCourse struct {
 	Id            int    `json:"id" gorm:"column:id"`
-	CourseTitle   string `json:"course_title" gorm:"column:course_title"`
-	CourseImage   string `json:"course_image" gorm:"column:course_image"`
-	CourseContent string `json:"course_content" gorm:"column:course_content"`
-	IsFree        bool   `json:"is_free" gorm:"column:is_free"`
-	IsActive      bool   `json:"is_active" gorm:"column:is_active"`
-	HasBuyer      bool   `json:"has_buyer" gorm:"column:has_buyer"`
+	CourseTitle   string `json:"courseTitle" gorm:"column:course_title"`
+	CourseImage   string `json:"courseImage" gorm:"column:course_image"`
+	CourseContent string `json:"courseContent" gorm:"column:course_content"`
+	IsFree        bool   `json:"isFree" gorm:"column:is_free"`
+	IsActive      bool   `json:"isActive" gorm:"column:is_active"`
+	HasBuyer      bool   `json:"hasBuyer" gorm:"column:has_buyer"`
 }
 
 type ReqCreateCourse struct {
 	Id            int    `json:"id" gorm:"column:id"`
-	CourseTitle   string `json:"course_title" gorm:"column:course_title"`
-	CourseImage   string `json:"course_image" gorm:"column:course_image"`
-	CourseContent string `json:"course_content" gorm:"column:course_content"`
-	IsFree        bool   `json:"is_free" gorm:"column:is_free"`
-	IsActive      bool   `json:"is_active" gorm:"column:is_active"`
-	HasBuyer      bool   `json:"has_buyer" gorm:"column:has_buyer"`
+	CourseTitle   string `json:"courseTitle" gorm:"column:course_title"`
+	CourseImage   string `json:"courseImage" gorm:"column:course_image"`
+	CourseContent string `json:"courseContent" gorm:"column:course_content"`
+	IsFree        bool   `json:"isFree" gorm:"column:is_free"`
+	IsActive      bool   `json:"isActive" gorm:"column:is_active"`
+	HasBuyer      bool   `json:"hasBuyer" gorm:"column:has_buyer"`
 }
